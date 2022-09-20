1679ab0f900e997324722b74eae66d2f350b092d
Monish Tandale <mtandale@apple.com>
2022-09-19 23:12:27 -0700

    Added menu functions to simpleList

--- simpleList/main.cpp
+++ simpleList/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-void printMenu(string name);
+void print_menu(string name);
+void print_list();
+void add_item();
+void delete_item();
+
+vector<string> list;
+string name;
 
 int main(int arg_count, char *args[]){
 
     if(arg_count>1){
-        string name(args[1]);
-        printMenu(name);
+        name = string(args[1]);
+        print_menu(name);
     }
     else{
         cout << "Username not supplied...exiting the program" << endl;
@@ -16,7 +25,7 @@ int main(int arg_count, char *args[]){
     return 0;
 }
 
-void printMenu(string name){
+void print_menu(string name){
     int choice;
 
     cout << "***************************\n";
@@ -31,7 +40,72 @@ void printMenu(string name){
     if(choice == 4){
        exit(0); 
     }
+    else if(choice == 2){
+        add_item();
+    }
+    else if(choice == 3){
+        delete_item();
+    }
+    else if(choice == 1){
+        print_list();
+    }
     else{
         cout << "Sorry choice not implemened yet.\n"; 
     }
+}
+
+void print_list(){
+    cout<< "\n\n\n\n";
+    cout<< "**** Printing List ****\n";
+
+    for(int i=0; i<list.size(); i++){
+        cout<< list[i] << "\n";
+    }
+
+    cout<< "\nM - Menu \n";
+    char choice;
+    cin>> choice;
+
+    if(choice == 'M' || choice == 'm'){
+        print_menu(name);
+    }
+    else{
+        cout<< "Invalid choice. Quitting...\n\n";
+    }
+}
+
+
+void add_item(){
+    cout<< "\n\n\n\n";
+    cout<< "**** Add Item ****\n";
+    cout<< "Type an item and press enter: ";
+
+    string item;
+    cin >> item;
+
+    list.push_back(item);
+
+    cout<<"Successfully added item to the list \n\n\n";
+    cin.clear();
+
+    print_menu(name);
+}
+
+void delete_item(){
+    cout<< "**** Delete Item ****\n";
+    cout<< "Select an item index number to delete: \n";
+
+    if(list.size()){
+        for(int i=0; i<list.size(); i++){
+            cout<< i << ": " << list[i] << endl;
+        }
+    } 
+    else{
+        cout<< "No items to delete.\n";
+    }
+
+    int index;
+    cin>> index;
+
+    print_menu(name);
 }

8796b5e88b81f4193992c7516c26a1c429a4b1ed
Monish Tandale <mtandale@apple.com>
2022-09-27 14:42:03 -0700

    complete version of simpleList program

--- simpleList/src/include/list.h
+++ simpleList/src/include/list.h
@@ -17,12 +17,17 @@ class List {
     ~List(){
         // Destructor
     }
+
+    vector<vector<string>> mainList;
     vector<string> list;
     string name;
+    int currentUserIndex;
 
     void print_menu();
     void print_list();
     void add_item();
-    void delete_item();    
+    void delete_item();   
+    bool find_userList(); 
+    void save_list();
  
 };
